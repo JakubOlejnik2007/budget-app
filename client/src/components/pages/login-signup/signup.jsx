5a5fd2f8ab57016a7f46f2385da3ef653fdd42d4
Szymon Sowiński <134802662+szymon-sowinski@users.noreply.github.com>
2023-10-30 21:47:43 +0100

    [Add feature]: Added sign-up form

--- client/src/components/pages/login-signup/signup.jsx
+++ client/src/components/pages/login-signup/signup.jsx
@@ -1,1 +1,20 @@
 import React from "react";
+import FormInput from "../../partials/form-input";
+
+const SignUpForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <FormInput id="imie" type="text" label="Imię" onChange={()=>{}} value="" />
+      <FormInput id="nazwisko" type="text" label="Nazwisko" onChange={()=>{}} value="" />
+      <FormInput id="email" type="email" label="Email" onChange={()=>{}} value="" />
+      <FormInput id="password" type="text" label="Hasło" onChange={()=>{}} value="" />
+      <FormInput id="confirm-password" type="text" label="Potwierdź hasło" onChange={()=>{}} value="" />
+    </form>
+  )
+}
+
+export default SignUpForm;
