a8dee79acebe2ff33b5a356d4aca09abf7a8cebd
Szymon Sowiński <134802662+szymon-sowinski@users.noreply.github.com>
2023-10-30 20:38:43 +0100

    Updated login form

--- client/src/components/pages/login-signup/login-signup.jsx
+++ client/src/components/pages/login-signup/login-signup.jsx
@@ -1,26 +1,11 @@
 import React from "react";
+import FormInput from "../../partials/form-input";
 
 const LoginSignup = () => {
-  const [toggle, setToglle] = useState;
+  const [toggle, setToglle] = useState(true);
 
   return <></>;
 };
 
-const LoginForm = () => {
-  return (
-    <form>
-      <p>
-        <label>
-          Adres e-mail: <input id="email" type="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Hasło: <input id="password" type="text" />
-        </label>
-      </p>
-      <button id="submit">Zaloguj</button>
-    </form>
-  );
-};
+
 export default LoginSignup;
