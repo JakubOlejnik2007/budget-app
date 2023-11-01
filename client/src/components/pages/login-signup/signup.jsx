b8e8b88ff409843e805b0fdc40920ab48eb1f12a
Szymon Sowiński <134802662+szymon-sowinski@users.noreply.github.com>
2023-11-01 19:26:07 +0100

    [Update feature]: Updated login and signup forms

--- client/src/components/pages/login-signup/signup.jsx
+++ client/src/components/pages/login-signup/signup.jsx
@@ -1,20 +1,20 @@
-import React from "react";
 import FormInput from "../../partials/form-input";
+import { signUpInputs } from "../../../utils/inputlists";
+import { Box } from "@mui/material";
 
 const SignupForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log("Submitted");
   }
 
-  return(
-    <form onSubmit={handleSubmit}>
-      <FormInput id="imie" type="text" label="Imię" onChange={()=>{}} value="" />
-      <FormInput id="nazwisko" type="text" label="Nazwisko" onChange={()=>{}} value="" />
-      <FormInput id="email" type="email" label="Email" onChange={()=>{}} value="" />
-      <FormInput id="password" type="text" label="Hasło" onChange={()=>{}} value="" />
-      <FormInput id="confirm-password" type="text" label="Potwierdź hasło" onChange={()=>{}} value="" />
-    </form>
-  )
+  return (
+    <Box component="form" onSubmit={handleSubmit}>
+      {
+        signpInputs.map((item, index) => <FormInput key={index} value="" {...item} onChange={() => { }} />)
+      }
+    </Box>
+  );
 }
 
 export default SignupForm;
