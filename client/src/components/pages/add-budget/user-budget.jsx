2b869f5f9cb5db53b0fe1c8b3be4a6c882a3f6a3
Szymon Sowiński <134802662+szymon-sowinski@users.noreply.github.com>
2023-11-05 21:33:41 +0100

    [Add feature] Added incomes and expenses forms

---
+++ client/src/components/pages/add-budget/user-budget.jsx
@@ -0,0 +1,35 @@
+import FormInput from "../../partials/form-input";
+import { budgetInputs } from "../../../utils/budgetinputs";
+import { loginInputs } from "../../../utils/inputlists";
+import { Box, Typography } from "@mui/material";
+import Button from '@mui/material/Button';
+
+const BudgetForm = () => {
+    return (
+        <>
+            <Typography variant="h1" sx={{
+                fontSize: "2rem",
+                textAlign: "center",
+                padding: "1rem 0",
+            }}>Dodaj przychód</Typography>
+            <Box component="form" sx={{
+                display: "flex",
+                flexDirection: "column",
+            }}>
+                {
+                    budgetInputs.map((item, index) => <FormInput key={index} value={loginFormValues[item.id]} {...item} onChange={onChange} />)
+                }
+                <Button type="submit" sx={{
+                    color: "#ffffff",
+                    backgroundColor: "#2884ec",
+                    margin: "auto",
+                    padding: "10px 20px",
+                    "&:hover": {
+                        backgroundColor: "#2aa4ec",
+                        color: "#000",
+                    }
+                }}>Dodaj</Button>
+            </Box>
+        </>
+    )
+}
